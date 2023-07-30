c51a0dbffdd98dd29021345169f59b3147a5e699
ezgi.turkokuloglu <ezgi.turkokuloglu@tubitak.gov.tr>
2023-07-30 16:21:05 +0300

    import

--- src/test/java/lifecycle/EmployeeLifeCyclePersistenceTest.java
+++ src/test/java/lifecycle/EmployeeLifeCyclePersistenceTest.java
@@ -12,8 +12,7 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.*;
 
 public class EmployeeLifeCyclePersistenceTest {
 
